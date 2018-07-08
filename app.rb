469e83e0b97eb6d5d5ef502814f04e6ccd8e1057
Spencer Goodman <spencerwgoodman@icloud.com>
2018-07-08 22:29:31 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -8,6 +8,14 @@ class App < Sinatra::Base
 
 
   post '/team' do
+    @team_name = params[:name]
+    @coach = params[:coach]
+    @pg = params[:pg]
+    @sg = params[:sg]
+    @sf = params[:sf]
+    @pf = params[:pf]
+    @c = params[:c]
+    
     erb :team
   end
 
